1835c441e5801e1833790efaa479e004472b8012
Arturo Solares <arturosolaresps@gmail.com>
2023-01-24 13:22:55 -0600

    acomodo de navbar entre desktop y mobile

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.burger-menu-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
-console.log('JS FUNCIONANDO');
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
